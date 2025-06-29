37976c20b01ecd98980496ce2c25870d33d3ab58
Yusuf Rais <ucupdevstuff@outlook.com>
2025-06-30 03:20:01 +0700

    Delete PongGame directory

--- PongGame/src/ucupandriska/ponggame/util/Time.java
+++
@@ -1,8 +0,0 @@
-package ucupandriska.ponggame.util;
-public class Time {
-    public static double timeStarted = System.nanoTime();
-
-    public static double getTime(){
-        return (System.nanoTime() - timeStarted) * 1E-9;
-    }
-}
